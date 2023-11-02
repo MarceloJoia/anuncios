8f4395b211ee9d367fffb94ff969b853b56ea307
Marcelo Joia <sitejoia@hotmail.com>
2023-11-02 09:15:39 -0300

    Preparando a entity Advert

--- app/Entities/Advert.php
+++ app/Entities/Advert.php
@@ -6,7 +6,56 @@ use CodeIgniter\Entity\Entity;
 
 class Advert extends Entity
 {
-    protected $datamap = [];
-    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
-    protected $casts   = [];
+    protected $dates   = ['created_at', 'updated_at', 'deleted_at', 'user_since'];
+    protected $casts   = [
+        'is_published'      => 'boolean',
+        'adverts'           => '?integer', // pode ou não ser null
+        'display_phone'     => 'boolean', // Exibe ou não o Telefone
+    ];
+
+
+    /**
+     * Preenche automaticamente a propriedade price [ Metodo Mágico ]
+     *
+     * @param string $price
+     * @return void
+     */
+    public function setPrice(string $price)
+    {
+        $this->attributes['price'] = str_replace(',', '', $price);
+    }
+
+
+    /**
+     * Esse método será utilizado pelo manager para publicar ou não um anúncio
+     *
+     * @param string $isPublished
+     * @return void
+     */
+    public function setIsPublished(string $isPublished)
+    {
+        $this->attributes['is_published'] = $isPublished ? true : false;
+    }
+
+    /**
+     * Recupera um anuncio arquivado
+     *
+     * @return void
+     */
+    public function recover()
+    {
+        $this->attributes['deleted_at'] = null;
+    }
+
+
+    /**
+     * Remove a propriedade address, images antes de manda para o banco de dados
+     *
+     * @return void
+     */
+    public function unsetAuxiliaryAttributes()
+    {
+        //unset($this->attributes['address']);
+        unset($this->attributes['images']);
+    }
 }
